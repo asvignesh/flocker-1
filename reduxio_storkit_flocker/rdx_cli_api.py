137f6625506e0b96bcc13e53f3365d7e5747baa7
pallitharadiljith <diljith@vitallabsinc.com>
2016-10-25 20:27:43 +0530

    Import Fix

--- reduxio_storkit_flocker/rdx_cli_api.py
+++ reduxio_storkit_flocker/rdx_cli_api.py
@@ -13,12 +13,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """Reduxio CLI intrface class for Reduxio Cinder Driver."""
+import paramiko
 import datetime
 import json
 import datetime
 import time
 import socket
-
 import logging
 
 from exceptions import *
